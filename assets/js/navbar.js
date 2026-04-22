a5e51ff7ca9b3512377720b6a86161b8090c04ac
Samara Alves <samaraalves778@gmail.com>
2026-04-21 22:54:13 -0300

    Delete assets/js/navbar.js

--- assets/js/navbar.js
+++
@@ -1,6 +0,0 @@
-// navbar.js
-// Hamburguer menu removido — navbar usa pills integradas.
-// Arquivo mantido para compatibilidade com app.js.
-function toggleHamburgerMenu() {}
-function fecharMenuOnClick() {}
-function setupNavbarEventListeners() {}
